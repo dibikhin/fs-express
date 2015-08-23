23eb23422a0f9b03f3e3a47f9f7d4a3ff27507db
Roman Dibikhin <dibikhin@gmail.com>
2015-08-23 13:56:04 +0300

    Update main.fs

--- main.fs
+++ main.fs
@@ -1,30 +1,73 @@
+//module FsExpress =
+//	type Status = Ok=200 | NotFound=404
+////	type ContentType = PlainText="asdf"
+//	
+//	type ContentType =
+//  		| PlainText
+//  		override this.ToString() =
+//    		match this with
+//    		| PlainText -> "Content-Type: text/plain"
+//	
+////	printfn "%O" ContentType.PlainText
+//	
+//	type Req = {Host:string; Route:string}
+//	
+//	type Response = {Data:string; Status:Status; ContentType:ContentType}
+//	
+//	let defaultResponse = {Data=""; Status=Status.Ok; ContentType=ContentType.PlainText}
+//	
+//	let send res =
+//		printf "%A" res
+//	
+//	let get = fun route handle ->
+//		let req = {Host="localhost"; Route="/home"}
+//		if route = req.Route then handle req "dummy"
+	
 module FsExpress =
-	type App = {route:string}
-	type Req = {host:string; route:string}
+	open System
+	open System.Net
+	open System.Text
+	open System.IO
+ 
+	let host = "http://localhost:8765"
+ 
+	let listener (route:string) (handler:(HttpListenerRequest->HttpListenerResponse->Async<unit>)) =
+		let hl = new HttpListener()
+		hl.Prefixes.Add (host + route)
+		hl.Start()
+		
+		let task = Async.FromBeginEnd(hl.BeginGetContext, hl.EndGetContext)
+		async {
+			while true do
+				let! context = task
+				Async.Start(handler context.Request context.Response)
+		} |> Async.Start
+		
+	let get (route:string) (txt:string) =
+		listener route (fun req resp ->
+			async {
+				let txtBytes = Encoding.ASCII.GetBytes(txt)
+//				let txt = Encoding.ASCII.GetBytes(DateTime.Now.ToString())				
+				resp.ContentType <- "text/html"
+				resp.OutputStream.Write(txtBytes, 0, txtBytes.Length)
+				resp.OutputStream.Close()
+			})
 	
-	let send text =
-		printfn text
-	
-	let send2 text code headers =
-		printf text
-		printf "%i"code
-		printf headers
-	
-	let get = fun route handle ->
-		let req = {host="localhost"; route="/home"}
-		if route = req.route then handle req "dummy"
-	
-module App = 
+module App =
 	open FsExpress
 	
-	get "/"
-		(fun req res ->
-			printf "%A" req.host
-			printf "%A" req.route
-			send "Hello!")
+	get "/" "Hi!"
 	
-	get "/home"
-		(fun req res ->
-			printf "%A" req.host
-			printf "%A" req.route
-			send2 "Hi, Home!" 200 "{'Content-Type': 'text/plain'}")
+	get "/home/" "hi, from home"
+	
+//	get "/"
+//		(fun req res ->
+//			printf "%A" req.Host
+//			printf "%A" req.Route
+//			send {defaultResponse with Data="Hello:)"})
+//	
+//	get "/home"
+//		(fun req res ->
+//			printf "%A" req.Host
+//			printf "%A" req.Route
+//			send {defaultResponse with Data="Hi, Home!"})
