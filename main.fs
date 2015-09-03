8428c9f449b9b514040e32d5deea21114599fc78
Roman Dibikhin <dibikhin@gmail.com>
2015-09-03 22:53:21 +0300

    Update main.fs

--- main.fs
+++ main.fs
@@ -11,37 +11,39 @@ module FsExpress =
 
 	type Handler = HttpListenerRequest->HttpListenerResponse->Async<unit>
 	
-	let defaultHost = "http://localhost:8769"
+	let defaultHost = "http://localhost:8760"
 	
 	let listener (route:string) (handler:Handler) =
 		let hl = new HttpListener()
 		let postfixedRoute = if route.EndsWith slash then route else (route + slash)
 		hl.Prefixes.Add (defaultHost + postfixedRoute)
-//		hl.Start()
-//		
-//		let task = Async.FromBeginEnd(hl.BeginGetContext, hl.EndGetContext)
-//		async {
-//			while true do
-//				let! context = task
-//				Async.Start(handler context.Request context.Response)
-//		} |> Async.Start
+		hl.Start()
 		
-	let get = fun route txt -> ignore
-//		listener route (fun req resp ->
-//			async {
-//				let txtBytes = Encoding.ASCII.GetBytes(txt:string)		
-//				resp.ContentType <- textHtml
-//				resp.OutputStream.Write(txtBytes, 0, txtBytes.Length)
-//				resp.OutputStream.Close()
-//			})
+		let task = Async.FromBeginEnd(hl.BeginGetContext, hl.EndGetContext)
+		async {
+			while true do
+				let! context = task
+				Async.Start(handler context.Request context.Response)
+		} |> Async.Start
+		
+	let get = fun route txt ->
+		listener route (fun req resp ->
+			let text = txt req
+			async {
+				let txtBytes = Encoding.ASCII.GetBytes(text:string)
+				resp.ContentType <- textHtml
+				resp.OutputStream.Write(txtBytes, 0, txtBytes.Length)
+				resp.OutputStream.Close()
+			})
 	
 module App =
 	open FsExpress
 	
-	get "/" "Hi!"
+	get "/" (fun req -> "Hi!")
 	
-	get "/home" "hi, from home"
+	get "/home" (fun req -> "hi, from home")
 	
-	get "/person" "hello, Person" // parse params
-	
+	get "/person" (fun req ->
+		req.QueryString.["oh"] |> Dump
+		"hello, Person")
 	// post "/person" // parse json to Person
